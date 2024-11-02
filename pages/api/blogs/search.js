30ea70d96fd9d5692e8a8b5f114caa0e5238ef90
root <root@LAPTOP-DQV0AUDB>
2024-11-02 18:38:24 -0400

    implementing sorting blog posts

--- pages/api/blogs/search.js
+++ pages/api/blogs/search.js
@@ -1,10 +1,27 @@
+/* Retrieve blog posts
+*/
 import prisma from "@/utils/db";
 
 export default async function handler(req, res) {
     if (req.method === 'GET') { // retrieve blog posts
         try {
-            const { page = 1, limit = 10} = req.query;
-            const searchQuery = req.query.query;
+            const pageNum = parseInt(req.query.page) || 1; // default to 1 
+            const pageSize = parseInt(req.query.pageSize) || 10; // default to 10
+            const searchQuery = req.query.query || '';
+            // if sortByRating is not set, posts are sorted by createAt ()
+            // const sortByRating = req.query.sortByRating === 'true'; //
+            const sortOption = req.query.sort;
+
+            let orderBy; 
+            if (sortOption === 'mostValuable') {
+                orderBy = { upvoteCount: 'desc', downvoteCount: 'asc', createdAt: 'desc' }; 
+            } else if (sortOption === 'mostControversial') {
+                orderBy = { downvoteCount: 'desc', createdAt: 'desc' };
+            } else {
+                orderBy = { createdAt: 'desc' }; // Default sort by creation date
+            }
+
+
             const blogPosts = await prisma.blogPost.findMany({ // use find many to get list of posts from db
                 where: { // conditions
                     OR: [ // OR allows matching of any of these fields (search could match title, description, tags)
@@ -13,7 +30,7 @@ export default async function handler(req, res) {
                         { description: { contains: searchQuery}},
                         { tags: {contains: searchQuery}}, 
                         {
-                        codeTemplates: {
+                            codeTemplates: {
                                 some: {
                                 title: { contains: searchQuery }
                                 }
@@ -22,10 +39,11 @@ export default async function handler(req, res) {
                     ],
                 },
                 include: { // relations
-                    codeTemplates: true
+                    codeTemplates: true         // fetch each blog post and all the related code templates stored in codeTemplatse field
                 },
                 skip: (page - 1) * limit,           // pagination offset
                 take: Number(limit),                // pagination limit
+                orderBy: orderBy, 
             }); 
             res.status(200).json(blogPosts);
         } catch (error) {
