8d2bead820e57ff45c1ab66765964ea475f2df49
root <root@LAPTOP-DQV0AUDB>
2024-11-03 21:59:20 -0500

    s

--- pages/api/blogs/index.js
+++ pages/api/blogs/index.js
@@ -127,7 +127,7 @@ export default async function handler(req, res) {
                     authorId,
                     codeTemplates: {
                         connect: codeTemplates ? codeTemplates.map(template => ({ id: template.id })) : [],
-                    }
+                    } // 
                 },
             });
             res.status(200).json(newBlogPost);
