590dba2c3c381dd1fa61c1cba6bf495466c5f5fc
York Ng <yorkhayng@gmail.com>
2024-11-15 17:45:49 -0500

    frontend setup

---
+++ components/pages/templates.tsx
@@ -0,0 +1,10 @@
+export default function TemplatesPage() {
+    return (
+        <div className="flex items-center justify-center h-screen text-textcolor">
+        <div className="w-96">
+            <h1 className="text-4xl font-bold mb-4">Templates</h1>
+            <p className="mb-4">This is a templates page.</p>
+        </div>
+        </div>
+    );
+}
